4a3638c9f74b748bac2f487e37d271b38b3e6ba9
JMAURICIORLIMA <mauricio.rodriguesdelima@gmail.com>
2021-10-11 17:01:01 -0300

    Config Cors

--- src/main/java/br/com/mv/main/config/WebConfig.java
+++ src/main/java/br/com/mv/main/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("https://projeto-front-mv.herokuapp.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
             .allowedHeaders("*");
     }
